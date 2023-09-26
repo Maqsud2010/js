6028ee83c39ffdc0fe3d70a5a31a58d15599bb9e
Maqsud2010 <maqsudjonmamanazarov3@gmail.com>
2023-09-26 18:00:11 +0500

    java

---
+++ app.js
@@ -0,0 +1,76 @@
+let prop = prompt ("Write billoner name");
+if (prop === "bill geyts"){
+    document.write(`
+    <div class="bill">
+    <div>
+    <img src="https://i.ytimg.com/vi/mXph1pCBhL4/maxresdefault.jpg"/>
+    </div>
+    <div>
+    <p>Bill Geyts kim? Bugungi kunda bu savolga javob bera olmaydigan odam topilmasa kerak. <br> Microsoft asoschisini hamma biladi, lekin Bill Geytsning biografiyasida siz bilmagan qiziqarli <br> ma'lumotlar bor.
+
+    • Bill Geyts boy oiladan tug'ilgan
+    Ko'p odamlar Bill Geyts oddiy oiladan deb o'ylaydi, <br> lekin noto'g'ri. Billning otasi mashhur advokat. Onasi o'qituvchi va "United Way International" moliyaviy <br> va telefon kompaniyasi direktorlar kengashi prezidenti.
+    
+    • 13 yoshida birinchi dasturini yozgan
+    13 yoshida <br> ota-onasi kichik Billni maxsus imtiyozli Lakeside maktabiga olib borganlar. Aytgancha, bu yerda u <br> kelajakdagi biznes sherigi Pol Allen (Microsoft korporatsiyasining asoschisi) bilan uchrashdi.
+    
+    <br> • U chiroyli qizlar bilan bir sinifda o'qish uchun dastur yaratdi
+    Bill Geytsning qobiliyatlari <br> haqida bilib olgach, maktab rahbariyati unga sinflar va jadvallarni avtomatik ravishda tuzadigan <br> dastur yozishni taklif qildi.</p>
+    </div>
+    </div>
+    `)
+}
+if(prop ==="steve jobs"){
+    document.write(`
+    <div class="steve">
+    <div>
+    <img src="https://ginnes.uz/sites/default/files/inline-images/steve-jobs-facts-adopted-658x362-1_0.jpg"/>
+    </div>
+    <div>
+    <p>Stiv Jobs 1955 yil 24 fevralda Abdulfatta Jandali va Joanna Shible oilasida tug’ilgan. Jandali islom <br> diniga mansub oiladan kelib chiqqan bo’lib, Shible esa katolik bo’lgan. Lekin Shible homilador <br> bo’lganida bu ikki yosh hali turmush qurishmagan edi.O’sha paytda ikkalasi ham Viskonsin <br> universitetida o’qishgan. Shible Kaliforniyaga tug’ilish uchun qochib ketgan. Shible bolani <br> o’zida qoldirmaslik qaroriga kelib, Jobsni boshqa oilaga bergan. Biroq, yillar o’tib, ular qiz <br> farzandli ham bo’lishgan va qizni o’zlarida qoldirishgan.Ko’pgina muvaffaqiyatli tadbirkorlarga o’xshab Jobs kollejdagi o’qishini tashlab ketgan. U 1972 yilda <br> Rida kollejiga o’qishga kiradi va birinchi semestrdan keyin o’qishni davom ettirishdan voz kechadi. <br> Ota-onasiga esa, o’zi uchun mantiqsiz ta’limga pul sarflashni istamasligi haqida aytadi. </p>
+    </div>
+    </div>
+    `)
+}
+if (prop ==="elon mask"){
+    document.write(`
+    <div class="elon">
+    <div><img src="https://cdn.xabardor.uz/media/photo/2022/07/05/news_photo-20220716-155626.webp"/></div>
+    <div>
+    <p>
+    Elon Reeve Musk (talaffuzi: Ilon Riv Mask; 1971-yil 28-iyunda tugʻilgan ) amerikalik investor,[2][3] muhandis[4] va <br> ixtirochidir[5][6][7][8]. U SpaceX asoschisi, bosh direktori va bosh muhandisi, Tesla Inc. va Neuralink asoschisi <br> va bosh direktori, SolarCity hamasoschisi va raisi, OpenAI hamraisi,Confinity bilan birlashib PayPal <br> nomini olgan X.com asoschisi Musk 2050-yilga qadar Marsga million kishini yuborishini, shuningdek, „SpaceX“ning sunʼiy yoʻldoshlar, ekipajlar <br> va yuklarni sayyoraga yetkazib berishga imkon beradigan transport tizimini ishlab chiqishni rejalashtirgan. <br> Britaniyalik astrofizik Martin Riz Dubayda boʻlib oʻtgan Butunjahon hukumatlar sammitidagi nutqida bu mavzuda <br> Elon Muskni tanqid qildi. Olimning soʻzlariga koʻra, Muskning ushbu gʻoyasi xavfli boʻlishi mumkin. Chunki <br> Marsda yashash Janubiy qutbda yoki Everest choʻqqisida yashashdan yaxshiroq emas. Elon Musk ajoyib <br> inson... Uning suhbatdoshi amerikalik astrofizik Nil Degrass Taysonning taʼkidlashicha, Marsda farovon hayot <br> kechirish uchun odamlar uni Yerga aylantirishi kerak. Bundan ham oldin aytish joizki, Yerdagi yuzaga kelishi <br> mumkin boʻlgan falokatdan qutilish uchun boshqa sayyoraga koʻchib oʻtish tobora haqiqatga toʻgʻri kelmaydigan <br> holatga aylanmoqda[16].
+    </p>
+    </div>
+    </div>
+    `)
+}
+if(prop === "zuckerberg"){
+    document.write(`
+    <div class="berg">
+    <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"/></div>
+    
+    <div>
+    <p>Zuckerbergning otasi yahudiy stomatolog boʻlib ishlagan, onasi esa — psixolog yahudiy boʻlgan. U maktab <br> yillarida kompyuter dasturiga ishqivoz edi, tarmoq versiyasida Risk oʻyinlarini ishlab chiqqan. Mark Microsoft <br> va AOL kompaniyalar ishga joylashish haqidagi taklifini rad etadi, u Harvard universitetining psixologiya <br> fakultetiga oʻqishga kiradi[4]. Mark Garvardda qoʻshimchasiga AT kurslariga ham qatnardi. <br><br>
+
+    Ayni oʻsha Harvard vaqtlari Chris Hughes bilan birgalikda Facebook ijtimoiy tarmoqni yaratishni boshlashadi. <br> Unga moliyaviy yordamni kelib chiqishi braziliyalik boʻlgan talaba Eduardo Saverin koʻrsatdi. Mark bir necha <br> bor hakerlikka qiziqishi boʻlgan.[5] Uning urinishlari Eduardo Saverin kompaniya boshqaruvidan chetlatdi, <br> oxir-oqibat 2005-yilda ishni sudda koʻrib chiqishgacha bordi. <br><br>
+   </p>
+    </div>
+    </div>
+    `)
+}
+if(prop === "brendan green"){
+    document.write(`
+    <div class="green">
+    <div><img src="https://assetsio.reedpopcdn.com/brendan-greene-im-done-with-battle-royale-1554127823463.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp"/></div>
+    <div>
+    <p>Brendan Grin 1976-yilda Irlandiyaning Ballyshennon shahrida tug’ilgan. U faoliyatini fotograflik <br> va «DJ» lik qilib boshlagan. 2013-yilda esa u video o’yinlar yaratishga va ularning dizayniga <br> qiziqib qoladi. Ko’plab izlanishlardan so’ng, u 2017-yilning dekabr oyida(to’liq) PUBG o’yinini <br> ommaga havola etadi. <br><br>
+
+    — PUBG o’yini dunyoda bir vaqtda eng ko’p o’ynalgan video o’yin hisoblanadi. 2017-yilning <br> dekabr oyida bu o’yin bir vaqtda 3 million 106 ming 358 kishi tomonidan o’ynalgan.<br>
+    — PUBG o’yinining eng yaxshi 2021-yilgi o’yinchisi Gonzo hisoblanadi.
+    — PUBG o’yini Koreya, Afg’oniston, Xitoy, Pokiston, Iordaniya, Nepal va Isroil davlatlarida taqiqlangan.<br>
+    
+    Ushbu o’yin asosochisi hozirda 44 yoshda. Ayoli bilan ajrashib ketgan. Qizi bilan yashaydi.</p>
+    </div>
+    </div>
+    `)
+}
